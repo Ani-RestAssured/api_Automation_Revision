6f7da2dea3718cf7c5eb78f6297d82fbd083e85d
Ani <anirudho747@gmail.com>
2024-09-04 21:21:19 +0530

    Firtsclass

--- src/test/java/classNotes/Class3_BasicWayOfGettingRequestBodyAndExtractingResponseUsingJsonPathAndUsingResponseForNextRequest.java
+++ src/test/java/classNotes/Class3_BasicWayOfGettingRequestBodyAndExtractingResponseUsingJsonPathAndUsingResponseForNextRequest.java
@@ -1,4 +1,4 @@
-package module4;
+package classNotes;
 
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
--- src/test/java/classNotes/Class6_SoftcodedWayOfGettingRequestBodyAndConvertingResponseToStringToJSON.java
+++ src/test/java/classNotes/Class6_SoftcodedWayOfGettingRequestBodyAndConvertingResponseToStringToJSON.java
@@ -1,4 +1,4 @@
-package module4;
+package classNotes;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
