8cd092647b7ab2beba5465746c4c53466c0411fb
Ani <anirudho747@gmail.com>
2024-09-04 21:18:29 +0530

    Firtsclass

---
+++ src/test/java/module4/Class91_RequestSpecAndSerializationAndResponseSpec.java
@@ -0,0 +1,62 @@
+package module4;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.ResponseSpecification;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.RestAssured;
+import pojo.AddPlace;
+import pojo.location;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class Class91_RequestSpecAndSerializationAndResponseSpec {
+
+    public static void main (String[] args)
+    {
+
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        AddPlace p = new AddPlace();
+        p.setAccuracy(50);
+        p.setName("Frontline house");
+        p.setPhone_number("(+91) 983 893 3937");
+        p.setAddress("29, side layout, cohen 09");
+        p.setWebsite("http://google.com");
+        p.setLanguage("French-IN");
+
+
+        List<String> myList = new ArrayList<String>();
+        myList.add(0,"shoe park");
+        myList.add(1,"shop");
+        p.setTypes(myList);
+
+        location l = new location();
+        l.setLat(-38.383494);
+        l.setLng(33.427362);
+        p.setLocation(l);
+
+        RequestSpecification spec = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").
+                addQueryParam("key","qaclick123").
+                setContentType(ContentType.JSON).
+                build();
+
+        RequestSpecification res = given().spec(spec).body(p);
+
+        ResponseSpecification respspec = new ResponseSpecBuilder().
+                expectStatusCode(200).
+                expectContentType(ContentType.JSON).
+                build();
+
+        Response response  = res.
+                when().post("/maps/api/place/add/json").
+                then().spec(respspec).extract().response();
+
+        String responseString = response.asString();
+        System.out.println(responseString);
+    }
+}
